b9af6506f0717158aa1d0f139659de3bacc90d2d
Lahn92 <lahn92@gmail.com>
2024-11-11 11:54:43 +0100

    merged to main

--- master.py
+++ master.py
@@ -1,18 +1,21 @@
-#This script will be used as the master scripts, that gets stated by a service file. and then starts all other scripts. 
-
 import subprocess
+import sys
+import os
+
+# Path to the virtual environment's Python executable
+venv_python = '/home/arcmetis/ArcMetis/.venv/bin/python3'
 
 # List of scripts to run
 scripts = [
     'UDPtoMQTT.py',
-    'winchController.py'
+    'winchController.py',
     'MQTTstatus.py'
 ]
 
 # Start each script in parallel
 processes = []
 for script in scripts:
-    process = subprocess.Popen(['python3', script])
+    process = subprocess.Popen([venv_python, script])
     processes.append(process)
 
 # Optionally wait for all processes to complete
