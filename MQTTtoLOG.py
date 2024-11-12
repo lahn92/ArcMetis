aa241961553457d62a2f0e457f9e57f0220d6c6a
Lahn92 <lahn92@gmail.com>
2024-11-12 12:05:31 +0100

    fixed log

--- MQTTtoLOG.py
+++ MQTTtoLOG.py
@@ -32,6 +32,7 @@ usb_present = os.path.exists(usb_base_path)
 # MQTT topic for USB status
 usb_status_topic = "status/noUSB"
 
+
 # Callback when a message is received
 def on_message(client, userdata, msg):
     global logging_enabled, csv_file_path
@@ -41,17 +42,18 @@ def on_message(client, userdata, msg):
     # Debug: print received topic and payload
     print(f"Received message: Topic = {topic}, Payload = {payload}")
 
-    # Start logging when "status/logging" is set to "1" and USB is present
+    # Start logging when "status/logging" is set to "1" or "1.0" and USB is present
     if topic == "status/logging":
-        if payload == "1" and not logging_enabled and usb_present:
+        if payload in ["1", "1.0"] and not logging_enabled and usb_present:
             logging_enabled = True
             # Create a new CSV file with UTC date and time in the name
             current_time = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
             csv_file_path = os.path.join(usb_base_path, f"data_log_{current_time}.csv")
             print(f"Logging started. Data will be saved to {csv_file_path}")
-        elif payload == "0" and logging_enabled:
+        elif payload in ["0", "0.0"] and logging_enabled:
             logging_enabled = False
             print("Logging stopped.")
+
 
     # Update data dictionary when probe topics are received
     elif logging_enabled and topic.startswith("probe/"):
