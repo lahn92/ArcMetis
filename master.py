53456a34af96aaf47bce4403657470bafe12d4d0
Lahn92 <lahn92@gmail.com>
2024-11-15 10:22:55 +0100

    delay to the start of MQTTstatus

--- master.py
+++ master.py
@@ -1,25 +1,30 @@
 import subprocess
-import sys
-import os
+import time
 
 # Path to the virtual environment's Python executable
 venv_python = '/home/arcmetis/ArcMetis/.venv/bin/python3'
 
-# List of scripts to run
+# List of scripts to run, excluding MQTTstatus.py for now
 scripts = [
     'UDPtoMQTT.py',
     'winchController.py',
-    'MQTTstatus.py',
     'MQTTtoLOG.py',
     'sonarToMQTT.py',
     'MAVtoMQTT.py',
 ]
 
-# Start each script in parallel
+# Start each script in parallel (except MQTTstatus.py)
 processes = []
 for script in scripts:
     process = subprocess.Popen([venv_python, script])
     processes.append(process)
+
+# Delay for 30 seconds
+time.sleep(30)
+
+# Start MQTTstatus.py
+mqtt_status_process = subprocess.Popen([venv_python, 'MQTTstatus.py'])
+processes.append(mqtt_status_process)
 
 # Optionally wait for all processes to complete
 for process in processes:
